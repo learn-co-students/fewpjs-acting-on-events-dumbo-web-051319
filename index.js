b28ce9cd035ca491e92ac7d9caa8514cde078b2c
Stefanos <stefanosu422@gmail.com>
2019-06-24 22:04:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+
+
+
+  const moveDodgerLeft = () => {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 2}px`;
+    }
+}
+
+
+    const moveDodgerRight = () => {
+        let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 14);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 4}px`;
+    
+        }
+    }
